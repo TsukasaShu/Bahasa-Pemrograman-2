8a94cb372480693f1d47c471d8b71ed93eebc060
TsukasaShu <fajarrizky043@gmail.com>
2025-03-02 02:15:59 +0700

    Delete LuasSegitiga.py

--- LuasSegitiga.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Masukkan Ukuran Alas Segitiga : "))
-t = float(input("Masukkan Ukuran Tinggi Segitiga : "))
-
-L = 0.5 * a * t
-
-print("Luas Segitiga Dengan Ukuran Alas", a, "Dan Dengan Ukuran Tinggi", t, "Adalah :", L)
