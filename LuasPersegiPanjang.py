8a6dd6b470584285da9b42f5c8fc31140545263d
TsukasaShu <fajarrizky043@gmail.com>
2025-03-02 02:10:29 +0700

    Add files via upload

---
+++ LuasPersegiPanjang.py
@@ -0,0 +1,6 @@
+p = float(input("Masukkan Ukuran Panjang : "))
+l = float(input("Masukkan Ukuran Lebar : "))
+
+luas = p * l
+
+print("Luas Persegi Panjang Dengan Ukuran Panjang", p, "Dan Dengan Ukuran Lebar", l, "Adalah :", luas)
