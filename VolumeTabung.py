5097024eba963077a1fcf09b9ac9579c46286bbf
TsukasaShu <fajarrizky043@gmail.com>
2025-03-02 15:54:26 +0700

    Delete VolumeTabung.py

--- VolumeTabung.py
+++
@@ -1,8 +0,0 @@
-import math
-
-r = float(input("Masukkan Ukuran Jari-jari Lingkaran Tabung : "))
-t = float(input("Masukkan Ukuran Tinggi Tabung : "))
-
-volume = math.pi * math.pow(r, 2) * t
-
-print("Volume Tabung Dengan Jari-jari Lingkaran", r, "Dan Dengan Tinggi", t, "Adalah :", volume)
